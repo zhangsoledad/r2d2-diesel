afb5d815c224e6ed7fd3f972fd9297dfc21ae8a3
zhangsoledad <787953403@qq.com>
2017-12-13 16:24:52 +0800

    bump r2d2 0.8

--- examples/mysql.rs
+++ examples/mysql.rs
@@ -4,13 +4,14 @@ extern crate r2d2_diesel;
 
 use std::thread;
 
-use diesel::pg::PgConnection;
+use diesel::mysql::MysqlConnection;
 use r2d2_diesel::ConnectionManager;
 
 fn main() {
-    let config = r2d2::Config::default();
-    let manager = ConnectionManager::<PgConnection>::new("postgres://localhost/");
-    let pool = r2d2::Pool::new(config, manager).expect("Failed to create pool.");
+    let manager = ConnectionManager::<MysqlConnection>::new("mysql://localhost/");
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
 
     for _ in 0..10i32 {
         let pool = pool.clone();
